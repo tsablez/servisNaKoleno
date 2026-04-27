c9230cb7f8bf000158b534507c2976465d487577
tsablez <me@tsablezub.ru>
2026-04-27 22:39:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function faq1 () {
+    console.log('Вы открыли детали 1го вопроса.');
+    answerOne.classList.toggle('hidden');
+    faqOne.classList.toggle('faq-selected');
+}
+function faq2 () {
+    console.log('Вы открыли детали 2го вопроса.');
+    answerTwo.classList.toggle('hidden');
+    faqTwo.classList.toggle('faq-selected');
+}
+function faq3 () {
+    console.log('Вы открыли детали 3го вопроса.');
+    answerThree.classList.toggle('hidden');
+    faqThree.classList.toggle('faq-selected');
+}
+function faq4 () {
+    console.log('Вы открыли детали 4го вопроса.');
+    answerFour.classList.toggle('hidden');
+    faqFour.classList.toggle('faq-selected');
+}
+function faq5 () {
+    console.log('Вы открыли детали 5го вопроса.');
+    answerFive.classList.toggle('hidden');
+    faqFive.classList.toggle('faq-selected');
+}
